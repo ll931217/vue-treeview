a01092b208223f96b7559764eafd24656f213eba
Liang-Shih Lin <liangshihlin@yahoo.com>
2019-02-11 08:56:16 +0800

    First Commit

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-const TreeView = {
-  install(Vue, options) {
-    Vue.mixin({
-      mounted() {
-        console.log('Mounted!')
-        if (options.called) {
-          console.log('Called!')
-        }
-      }
-    })
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faPlusSquare,
+  faMinusSquare,
+  faPlus,
+  faMinus,
+  faTruckLoading
+} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import TreeView from './treeview.vue'
+
+library.add(faPlusSquare)
+library.add(faMinusSquare)
+library.add(faPlus)
+library.add(faMinus)
+library.add(faTruckLoading)
+
+const Tree = {
+  install(Vue) {
+    Vue.component('treeview', TreeView)
+    Vue.component('fa', FontAwesomeIcon)
   }
 }
+
+export default Tree
